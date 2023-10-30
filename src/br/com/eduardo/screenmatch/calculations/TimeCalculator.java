d71f2094140eb5bf1661128f3ec6b8f79f3d328a
Eduardo0608 <eduardoaguliari0608@gmail.com>
2023-10-30 13:37:38 -0300

    docs: adding comments in TimeCalculator class

--- src/br/com/eduardo/screenmatch/calculations/TimeCalculator.java
+++ src/br/com/eduardo/screenmatch/calculations/TimeCalculator.java
@@ -5,6 +5,8 @@ import br.com.eduardo.screenmatch.models.Title;
 public class TimeCalculator {
     private int totalTime = 0;
 
+
+    // Teste de documentação com conventional commit.
     public void includes(Title t) {
         System.out.println("Adding duration in minutes for " + t.getName() + "...");
         this.totalTime += t.getDurationInMinutes();
